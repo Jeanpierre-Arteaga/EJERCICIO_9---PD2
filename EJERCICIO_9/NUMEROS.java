6d85647642909e8d1a09c0b3f9567766aeae11d6
Jeanpierre-Arteaga <jeanpierre.arteaga@unmsm.edu.pe>
2024-09-24 11:59:02 -0500

    Add files via upload

---
+++ EJERCICIO_9/NUMEROS.java
@@ -0,0 +1,29 @@
+package PRACTICA_DOMICILIARIA_02.EJERCICIO_9;
+
+public class NUMEROS{
+    private int numero;
+    private int opuesto;
+    //Constructores
+    public NUMEROS(){
+    }
+    public NUMEROS(int numero){
+        this.numero=numero;
+        this.opuesto= (187-numero);
+    }
+    //Setters
+    public void setNumero(int numero){
+        this.numero=numero;
+
+    }
+    public void setOpuesto(int numero){
+        this.opuesto= (187-numero);
+
+    }
+    //Getters
+    public int getOpuesto(){
+        return opuesto;
+    }
+    public int getNumero(){
+        return numero;
+    }
+}
